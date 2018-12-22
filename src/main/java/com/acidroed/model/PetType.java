13b6d3a4661c78cae7469c5e7dd627a408c91e79
oleksii.deordytsia@infopulse.com <oleksii.deordytsia@infopulse.com>
2018-12-22 13:18:23 +0200

    init POJOs

---
+++ src/main/java/com/acidroed/model/PetType.java
@@ -0,0 +1,17 @@
+package com.acidroed.model;
+
+/**
+ * Created by Oleksii.Deordytsia on 22.12.2018.
+ */
+public class PetType {
+
+		private String name;
+
+		public String getName() {
+				return name;
+		}
+
+		public void setName(String name) {
+				this.name = name;
+		}
+}
